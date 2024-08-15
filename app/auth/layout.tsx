ca29eb8874fd18dfa4c27eba68642d2d997ea9d3
Pablo George <pablogeokar@hotmail.com>
2024-08-15 20:43:01 -0300

    iniciando o layout da tela de login

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,7 +2,9 @@ import { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center">{children}</div>
+    <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      {children}
+    </div>
   );
 };
 
