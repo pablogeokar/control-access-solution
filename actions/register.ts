8670b4a0ba53f16af78e118475c15fb7e3b08af7
Pablo George <pablogeokar@hotmail.com>
2024-08-17 11:51:38 -0300

    iniciando a integração com o banco de dados e criando a rotina para inserção de usuário

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,8 @@
 "use server";
 import * as z from "zod";
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -8,6 +10,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: "Invalid fields" };
   }
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingUser = await db.user.findUnique({ where: { email } });
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
 
   return { success: "Email sent!" };
 };
